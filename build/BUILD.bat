22a2b1e13a81363e2ce4cee2ac97a93a7f8a7c8d
Kyle Spofford <kts9408@gmail.com>
2018-12-09 15:08:51 -0500

    more title

--- build/BUILD.bat
+++ build/BUILD.bat
@@ -2,4 +2,4 @@
 set CommonCompilerFlags=-MTd -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -Zi
 del *.pdb > NUL 2> NUL
 cl %CommonCompilerFlags% ..\src\FluffyFortnight.cpp -FmFluffyFortnight.map /LD /link -incremental:no -opt:ref -PDB:FluffyFortnight-%random%.pdb /DLL -EXPORT:renderGameGfx -EXPORT:initGame -EXPORT:renderGameAudio -EXPORT:updateGame
-cl %CommonCompilerFlags% ..\src\win32_FluffyFortnight.cpp -Fmwin32_FluffyFortnight.map user32.lib gdi32.lib
+cl %CommonCompilerFlags% ..\src\win32_FluffyFortnight.cpp -Fmwin32_FluffyFortnight.map /link -PDB:win32_FluffyFortnight.pdb user32.lib gdi32.lib
